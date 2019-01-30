52c5365a8dc63fa04df6a05389931f1c606b46fc
jainpriya <priya.mca16.du@gmail.com>
2019-01-30 10:38:52 +0530

    changes

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,9 +1,16 @@
 class UrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
+    flash[:notice] = ""
+    flash[:error] = ""
+  end
+  #shows the view of short_url generated from long_url
+  #Params:[:id]
+  def show
   end
   #Generates a new form 
   def new
+    flash[:notice] = ""
     @url = Url.new
   end
   #Create a short_url for given long_url
@@ -29,10 +36,7 @@ class UrlsController < ApplicationController
         end
     end
   end
-  #shows the view of short_url generated from long_url
-  #Params:[:id]
-  def show
-  end
+  
   #Gets a long_url from short_url from database
   #Params:short_url in json and html format
   #Request Type:Get
@@ -42,9 +46,7 @@ class UrlsController < ApplicationController
     @url = Url.new(url_params)
     @url.short_url = sanitize(@url.short_url)
     respond_to do |format|
-      @url_find = Rails.cache.fetch(@url.short_url,expires_in: 12.0.hours) do
-          Url.find_by_short_url(@url.short_url)
-          end
+      @url_find = Rails.cache.fetch(@url.short_url,expires_in: 12.0.hours){Url.find_by_short_url(@url.short_url)}
       @url.long_url = @url_find.nil? ? "Not found" : @url_find.long_url
       if @url.long_url == "Not found"
         @url.long_url = ""
@@ -63,17 +65,17 @@ class UrlsController < ApplicationController
   #Params:Long_url given by the user in new form
   #Output:santized url with stripped out https://www.
   def sanitize(long_url)
-      long_url.strip!
-      sanitized_url = long_url.downcase.gsub(/(https?:\/\/)|(www\.)|(http:\/\/)/, "")
-      sanitized_url.strip!
-      while(sanitized_url[0] == "/")
-        sanitized_url.slice!(0)
-      end
-      return sanitized_url
+    long_url.strip!
+    sanitized_url = long_url.downcase.gsub(/(https?:\/\/)|(www\.)|(http:\/\/)/, "")
+    sanitized_url.strip!
+    while(sanitized_url[0] == "/")
+      sanitized_url.slice!(0)
+    end
+    return sanitized_url
   end
   #Only permits those params that are required.Done for safety purpose
   def url_params
-        params.require(:url).permit(:long_url, :short_url)
-    end
+    params.require(:url).permit(:long_url, :short_url)
+  end
 
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,67 +1,70 @@
 class Url < ApplicationRecord
   searchkick word_start: [:short_url]
   require 'elasticsearch/model'
+  require "redis"
+  require 'date'
+  require 'digest/sha1'
+  require 'base64'
   include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
-    #Creates a Dynamic indexing on the basis of short_url
+  include Elasticsearch::Model::Callbacks
+  #Creates a Dynamic indexing on the basis of short_url
   settings do
     mappings dynamic: false do
       indexes :short_url, type: :text
+      indexes :long_url,type: :text
     end
   end
-  require "redis"
-  require 'date'
+
   index_name('urls') #Name of elastic search index
   after_commit :increment_count_generated_url 
-  require 'digest/sha1'
-  require 'base64'
+  
   validates :long_url, presence: true, on: :create
-    validates_format_of :long_url,
-    with: /\A(?:(?:http|https):\/\/*)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
-    #Increments the count of newly generated short_url every time a new _url is genearted and saved in database
-    def increment_count_generated_url
-      $redis.incr (Date.today)
+  validates_format_of :long_url,
+  with: /\A(?:(?:http|https):\/\/*)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
+  #Increments the count of newly generated short_url every time a new _url is genearted and saved in database
+  def increment_count_generated_url
+    $redis.incr (Date.today)
+  end
+  #Gives a short Url and returns the genearted url or Invalid Url
+  def self.shorten_url(long_url)
+    @url = Url.new
+    @url.long_url = long_url
+    @url.short_url = self.generate_short_url(long_url)
+    if(@url.save)
+      @url_find = Url.find_by_long_url(@url.long_url)
+      resp = @url_find.short_url
+      #Write in cache
+      Rails.cache.write(@url.long_url,@url_find)
+    else
+      resp = "Invalid Url"
     end
-    #Gives a short Url and returns the genearted url or Invalid Url
-    def self.shorten_url(long_url)
-      @url = Url.new
-      @url.long_url = long_url
-      @url.short_url = self.generate_short_url(long_url)
-      if(@url.save)
-        @url_find = Url.find_by_long_url(@url.long_url)
-        resp = @url_find.short_url
-        #Write in cache
-        Rails.cache.write(@url.long_url,@url_find)
-      else
-        resp = "Invalid Url"
+  return resp
+  end
+  #Encodes the long_url to short_url
+  #Params:Long_url
+  #Output:Encoded short_url
+  def self.generate_short_url(long_url)
+    regex_for_domain=/.*\./
+    domain_name = long_url.match(regex_for_domain).to_s
+    domain_name = self.encrypt_domain_name(domain_name)
+    id_for_encryption = (Url.last.id+1).to_s
+    encrypted_id = Digest::MD5.hexdigest(Base64.encode64(id_for_encryption))[0,3]
+    short_url = domain_name+'/'+encrypted_id
+    return short_url
+  end
+  #Encrypts domain name 
+  def self.encrypt_domain_name(domain_name)
+    case domain_name
+    when "housing."
+      domain_name = "hsg.com"
+    when "makaan."
+      domain_name = "mkn.com"
+    when "proptiger."
+      domain_name = "ptr.com"
+    else
+      domain_name = domain_name[0,3]+".com"
     end
-    return resp
-    end
-    #Encodes the long_url to short_url
-    #Params:Long_url
-    #Output:Encoded short_url
-    def self.generate_short_url(long_url)
-      regex_for_domain=/.*\./
-      domain_name = long_url.match(regex_for_domain).to_s
-      domain_name = self.encrypt_domain_name(domain_name)
-      id_for_encryption = Url.last.id.to_s
-      encrypted_id = Digest::MD5.hexdigest(Base64.encode64(id_for_encryption))[0,3]
-      short_url = domain_name+'/'+encrypted_id
-      return short_url
-    end
-    #Encrypts domain name 
-    def self.encrypt_domain_name(domain_name)
-      case domain_name
-      when "housing."
-        domain_name = "hsg.com"
-      when "makaan."
-        domain_name = "mkn.com"
-      when "proptiger."
-        domain_name = "ptr.com"
-      else
-        domain_name = domain_name[0,3]+".com"
-      end
-      return domain_name
-    end
+    return domain_name
+  end
 end
 
